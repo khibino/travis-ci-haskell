f578e80e300e4bd7d99b389a82a8aa81a153f826
Kei Hibino <ex8k.hibino@gmail.com>
2015-03-24 20:12:28 +0900

    Update to save errors.

--- tests/UseQuickCheck2.hs
+++ tests/UseQuickCheck2.hs
@@ -2,20 +2,33 @@
 import TestDatas (int1, stringHello)
 
 import Control.Applicative ((<$>))
-import Data.Monoid (Monoid (..), Last (..), mconcat)
+import Control.Monad (unless)
+import Data.Maybe (catMaybes)
+import System.IO (stderr, hPutStrLn)
 import Test.QuickCheck
   (Testable, Property, Result (..), quickCheckResult, label)
 
 
-lastError :: Result -> Last Result
-lastError = d  where
-  d (Success {})  =  Last   Nothing
-  d x             =  Last $ Just x
+type Test = (String, Property)
 
-run :: Testable prop => [prop] -> IO ()
+testProperty :: Testable prop => String -> prop -> Test
+testProperty m = (,) m . label m
+
+runMayError :: Testable prop => (a, prop) -> IO (Maybe (a, Result))
+runMayError (m, prop) = fmap ((,) m) . err <$> quickCheckResult prop  where
+  err :: Result -> Maybe Result
+  err (Success {})  =  Nothing
+  err x             =  Just x
+
+run :: [Test] -> IO ()
 run xs = do
-  e <- getLast . mconcat <$> mapM (fmap lastError . quickCheckResult) xs
-  maybe (return ()) (fail . show) e
+  es <- catMaybes <$> mapM runMayError xs
+  mapM_ (\(m, r) -> hPutStrLn stderr $ m ++ ":\n" ++ show r) es
+  unless (null es) $ fail "Found some failures."
+
+main :: IO ()
+main = run tests
+
 
 prop_int1 :: Bool
 prop_int1 = int1 == 1
@@ -26,11 +39,10 @@ prop_stringHello = stringHello == "Hello"
 prop_show_read :: Int -> Bool
 prop_show_read i = read (show i) == (i :: Int)
 
-tests :: [Property]
-tests =  [ label "int1"         prop_int1
-         , label "stringHello"  prop_stringHello
-         , label "show read"    prop_show_read
+tests :: [Test]
+tests =  [ testProperty "int1"         prop_int1
+         , testProperty "stringHello"  prop_stringHello
+         -- -- , testProperty "int1Bad"         $ int1 == 2
+         -- -- , testProperty "stringHelloBad"  $ stringHello == "Hellox"
+         , testProperty "show read"    prop_show_read
          ]
-
-main :: IO ()
-main =  run tests
