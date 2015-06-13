848ccff914126653ca7d2c307375e401be8e71f2
Kei Hibino <ex8k.hibino@gmail.com>
2015-06-13 19:34:01 +0900

    Switch to quickcheck-simple.

--- tests/UseQuickCheck2.hs
+++ tests/UseQuickCheck2.hs
@@ -1,33 +1,8 @@
 
 import TestDatas (int1, stringHello)
 
-import Control.Applicative ((<$>))
-import Control.Monad (when, unless)
-import Data.Maybe (catMaybes)
-import System.IO (stderr, hPutStrLn)
-import Test.QuickCheck
-  (Testable, Property, Result (..), quickCheckResult, label)
+import Test.QuickCheck.Simple (Test, boolTest, qcTest, defaultMain)
 
-
-type Test = (String, Property)
-
-testProperty :: Testable prop => String -> prop -> Test
-testProperty m = (,) m . label m
-
-runMayError :: Testable prop => (a, prop) -> IO (Maybe (a, Result))
-runMayError (m, prop) = fmap ((,) m) . err <$> quickCheckResult prop  where
-  err :: Result -> Maybe Result
-  err (Success {})  =  Nothing
-  err x             =  Just x
-
-defaultMain' :: Bool -> [Test] -> IO ()
-defaultMain' verbose xs = do
-  es <- catMaybes <$> mapM runMayError xs
-  when verbose $ mapM_ (\(m, r) -> hPutStrLn stderr $ m ++ ":\n" ++ show r) es
-  unless (null es) $ fail "Found some failures."
-
-defaultMain :: [Test] -> IO ()
-defaultMain = defaultMain' False
 
 main :: IO ()
 main = defaultMain tests
@@ -43,9 +18,7 @@ prop_show_read :: Int -> Bool
 prop_show_read i = read (show i) == (i :: Int)
 
 tests :: [Test]
-tests =  [ testProperty "int1"         prop_int1
-         , testProperty "stringHello"  prop_stringHello
-         -- -- , testProperty "int1Bad"         $ int1 == 2
-         -- -- , testProperty "stringHelloBad"  $ stringHello == "Hellox"
-         , testProperty "show read"    prop_show_read
+tests =  [ boolTest "int1"         prop_int1
+         , boolTest "stringHello"  prop_stringHello
+         , qcTest    "show read"   prop_show_read
          ]
