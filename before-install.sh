231420ad5fe84f60f1302bf097f0ffafef0ae971
Kei Hibino <ex8k.hibino@gmail.com>
2017-01-30 21:45:07 +0900

    try stack.

---
+++ before-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+set -x
+
+unset CC
+if [ x"$STACK_RESOLVER" != x ]; then
+    mkdir -p ~/.local/bin
+    PATH=$HOME/.local/bin:$PATH
+    export PATH
+    travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+else
+    PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
+    export PATH
+    case "$DEBIANVER" in
+        wheezy)
+            export CABAL_CONSTRAINTS='--constraint=transformers==0.3.0.0 --constraint=containers==0.4.2.1 --constraint=bytestring==0.9.2.1 --constraint=text==0.11.2.0 --constraint=time==1.4 --constraint=dlist==0.5 --constraint=convertible==1.0.11.1 --constraint=HDBC==2.3.1.1' ;
+            travis_retry cabal update -v ;
+            travis_retry cabal install $CABAL_CONSTRAINTS text dlist HDBC ;
+            ;;
+        '')
+            ;;
+        *)
+            echo "Unsupported DEBIANVER: $DEBIANVER"
+            exit 1
+            ;;
+    esac
+fi
