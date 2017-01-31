1fc346526a4584d609427f72576a64c9389cf4d6
Kei Hibino <ex8k.hibino@gmail.com>
2017-02-01 00:50:27 +0900

    move body for multi-directory.

--- travis-CI/cabal-hvr-ghc/script.sh
+++ travis-CI/cabal-hvr-ghc/script.sh
@@ -3,15 +3,19 @@
 set -e
 set -x
 
-if [ -f configure.ac ]; then autoreconf -i; fi
-cabal configure $CABAL_CONSTRAINTS --enable-tests --enable-benchmarks -v2  # -v2 provides useful information for debugging
-cabal build   # this builds all libraries and executables (including tests/benchmarks)
-cabal test
-cabal check
-cabal sdist   # tests that a source-distribution can be generated
+script_build() {
+    if [ -f configure.ac ]; then autoreconf -i; fi
+    cabal configure $CABAL_CONSTRAINTS --enable-tests --enable-benchmarks -v2  # -v2 provides useful information for debugging
+    cabal build   # this builds all libraries and executables (including tests/benchmarks)
+    cabal test
+    cabal check
+    cabal sdist   # tests that a source-distribution can be generated
 
-# Check that the resulting source distribution can be built & installed.
-# If there are no other `.tar.gz` files in `dist`, this can be even simpler:
-# `cabal install --force-reinstalls dist/*-*.tar.gz`
-SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz && \
-    (cd dist && cabal install --force-reinstalls "$SRC_TGZ")
+    # Check that the resulting source distribution can be built & installed.
+    # If there are no other `.tar.gz` files in `dist`, this can be even simpler:
+    # `cabal install --force-reinstalls dist/*-*.tar.gz`
+    SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz && \
+        (cd dist && cabal install --force-reinstalls "$SRC_TGZ")
+}
+
+script_build
