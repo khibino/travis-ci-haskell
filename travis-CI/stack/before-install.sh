2866de33b4888766e2348a9882b0666f63763137
Kei Hibino <ex8k.hibino@gmail.com>
2017-01-31 14:33:21 +0900

    fix for stack.yaml.

--- travis-CI/stack/before-install.sh
+++ travis-CI/stack/before-install.sh
@@ -9,4 +9,7 @@ set -x
 mkdir -p ~/.local/bin
 custom_retry curl -L https://www.stackage.org/stack/linux-x86_64 \
     | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
-sed "s/^resolver: .*/resolver: ${STACK_RESOLVER}/" < travis-CI/stack/template.yaml > travis-CI/stack/stack.yaml
+sed "s/^resolver: .*/resolver: ${STACK_RESOLVER}/" \
+    < travis-CI/stack/template.yaml \
+    > stack-travis.yaml
+##    stack.yaml must be located the same directory which has *.cabal -- constraint of stack?
