429f0ba1733235bf72a89ec7d30c05607ccc8094
Kei Hibino <ex8k.hibino@gmail.com>
2017-01-31 11:19:16 +0900

    install dependencies for tests.

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ x"$STACK_RESOLVER" != x ]; then
           || ( cd /home/travis/.stack/programs/x86_64-linux/ghc-8.0.1.temp/ghc-8.0.1/ \
                      && /home/travis/.stack/programs/x86_64-linux/ghc-8.0.1.temp/ghc-8.0.1/configure \
                             --prefix=/home/travis/.stack/programs/x86_64-linux/ghc-8.0.1 )
-    stack --resolver "$STACK_RESOLVER" install --only-dependencies
+    stack --resolver "$STACK_RESOLVER" install --test --only-dependencies
 else
     cabal --version
     echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
