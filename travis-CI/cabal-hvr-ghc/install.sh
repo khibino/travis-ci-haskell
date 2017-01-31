dbdaa14a09694323ec4f78dd29f6ca4ef6c883f1
Kei Hibino <ex8k.hibino@gmail.com>
2017-01-31 15:12:59 +0900

    divide cache dir per cabal version.

--- travis-CI/cabal-hvr-ghc/install.sh
+++ travis-CI/cabal-hvr-ghc/install.sh
@@ -18,26 +18,27 @@ sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
 cabal install $CABAL_CONSTRAINTS --only-dependencies --enable-tests --enable-benchmarks --dry -v > installplan.txt
 sed -i -e '1,/^Resolving /d' installplan.txt; cat installplan.txt
 
+cabsnap_dir=$HOME/.cabsnap/s-${CABALVER}
+
 # check whether current requested install-plan matches cached package-db snapshot
-# - if diff -u installplan.txt $HOME/.cabsnap/installplan.txt;
-if diff -u $HOME/.cabsnap/installplan.txt installplan.txt;
+if diff -u ${cabsnap_dir}/installplan.txt installplan.txt;
 then
     echo "cabal build-cache HIT";
     rm -rfv .ghc;
-    cp -a $HOME/.cabsnap/ghc $HOME/.ghc;
-    cp -a $HOME/.cabsnap/lib $HOME/.cabsnap/share $HOME/.cabsnap/bin $HOME/.cabal/;
+    cp -a ${cabsnap_dir}/ghc $HOME/.ghc;
+    cp -a ${cabsnap_dir}/lib ${cabsnap_dir}/share ${cabsnap_dir}/bin $HOME/.cabal/;
 else
     echo "cabal build-cache MISS";
-    rm -rf $HOME/.cabsnap;
+    rm -rf ${cabsnap_dir};
     mkdir -p $HOME/.ghc $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin;
     cabal install $CABAL_CONSTRAINTS --only-dependencies --enable-tests --enable-benchmarks;
 fi
 
 # snapshot package-db on cache miss
-if [ ! -d $HOME/.cabsnap ];
+if [ ! -d ${cabsnap_dir} ];
 then
     echo "snapshotting package-db to build-cache";
-    mkdir $HOME/.cabsnap;
-    cp -a $HOME/.ghc $HOME/.cabsnap/ghc;
-    cp -a $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin installplan.txt $HOME/.cabsnap/;
+    mkdir -p ${cabsnap_dir};
+    cp -a $HOME/.ghc ${cabsnap_dir}/ghc;
+    cp -a $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin installplan.txt ${cabsnap_dir}/;
 fi
