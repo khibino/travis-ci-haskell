97afa748f53458b74e7281398db78e98c445be04
Kei Hibino <ex8k.hibino@gmail.com>
2017-01-31 14:22:13 +0900

    update expanded yaml path.

--- travis-CI/stack/before-install.sh
+++ travis-CI/stack/before-install.sh
@@ -9,4 +9,4 @@ set -x
 mkdir -p ~/.local/bin
 custom_retry curl -L https://www.stackage.org/stack/linux-x86_64 \
     | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
-sed "s/^resolver: .*/resolver: ${STACK_RESOLVER}/" < travis-CI/stack/template.yaml > stack.yaml
+sed "s/^resolver: .*/resolver: ${STACK_RESOLVER}/" < travis-CI/stack/template.yaml > travis-CI/stack/stack.yaml
