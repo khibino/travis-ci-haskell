e4c88ac465d1d05b6acdc6fa01b41827466d85b8
Kei Hibino <ex8k.hibino@gmail.com>
2017-01-31 10:22:11 +0900

    rename extension along with public stack.

--- before-install.sh
+++ before-install.sh
@@ -10,7 +10,7 @@ unset CC
 if [ x"$STACK_RESOLVER" != x ]; then
     mkdir -p ~/.local/bin
     custom_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
-    sed "s/^resolver: .*/resolver: ${STACK_RESOLVER}/" < stack-template.yml > stack-travis.yml
+    sed "s/^resolver: .*/resolver: ${STACK_RESOLVER}/" < stack-template.yaml > stack-travis.yaml
 else
     case "$DEBIANVER" in
         wheezy)
