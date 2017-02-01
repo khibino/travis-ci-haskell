d54fabcd315698ab4e5e2672dd4849eee31e4b89
Kei Hibino <ex8k.hibino@gmail.com>
2017-02-01 23:53:51 +0900

    link time package to test binary.

--- pkg-a/tests/useQuickCheckSimple.hs
+++ pkg-a/tests/useQuickCheckSimple.hs
@@ -12,7 +12,7 @@ prop_int1 :: Bool
 prop_int1 = int1 == 1
 
 prop_stringHello :: Bool
-prop_stringHello = stringHello == "Hello"
+prop_stringHello = stringHello == "Hello 2017-01-02 12:34:56"
 
 prop_show_read :: Int -> Bool
 prop_show_read i = read (show i) == (i :: Int)
