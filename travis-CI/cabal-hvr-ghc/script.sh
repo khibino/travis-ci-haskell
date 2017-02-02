8b547d92ffa343629aebc7bc86c7306f309bd686
Kei Hibino <ex8k.hibino@gmail.com>
2017-02-02 12:30:35 +0900

    handle new variable.

--- travis-CI/cabal-hvr-ghc/script.sh
+++ travis-CI/cabal-hvr-ghc/script.sh
@@ -9,7 +9,7 @@ set -x
 script_build() {
     if [ -f configure.ac ]; then autoreconf -i; fi
     cabal configure $CABAL_CONSTRAINTS --enable-tests --enable-benchmarks -v2  # -v2 provides useful information for debugging
-    cabal build $CABAL_JOBS  # this builds all libraries and executables (including tests/benchmarks)
+    cabal build $CABAL_JOBS $CABAL_GHC_JOBS  # this builds all libraries and executables (including tests/benchmarks)
     cabal test $CABAL_JOBS
     cabal check
     cabal sdist   # tests that a source-distribution can be generated
@@ -18,7 +18,7 @@ script_build() {
     # If there are no other `.tar.gz` files in `dist`, this can be even simpler:
     # `cabal install --force-reinstalls dist/*-*.tar.gz`
     SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz && \
-        (cd dist && cabal install $CABAL_JOBS --force-reinstalls "$SRC_TGZ")
+        (cd dist && cabal install $CABAL_JOBS $CABAL_GHC_JOBS --force-reinstalls "$SRC_TGZ")
 }
 
 if [ x"$dirs" = x ]; then
